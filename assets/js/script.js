45af826579ceff959b9855388b4f51aeb7ead1c7
tbressel <tbressel.dev@gmail.com>
2023-09-01 20:02:13 +0200

    Footer menu and burger menu done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+// burger menu
+    const burgerMenu = document.querySelector(".burger");
+    burgerMenu.addEventListener('click', () => {
+        document.getElementById("nav__container").classList.toggle("nav-show")
+    })
+
+
+
+
+
+
+// Footer menu 
     const footerMenuItems = document.querySelectorAll(".js-footer-menu");
-  
     footerMenuItems.forEach(menuItem => {
         menuItem.addEventListener('click', () => {
-           menuItem.nextElementSibling.classList.toggle("hidden")
+            menuItem.nextElementSibling.classList.toggle("hidden")
         });
     });
-    
 });
