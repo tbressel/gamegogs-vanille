076273f71963a92c93d479e0b5e8ee44cfa62190
tbressel <tbressel.dev@gmail.com>
2023-09-01 23:33:41 +0200

    index page wallpaper on each DOM loading. Elements of the body

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,24 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-// burger menu
+    // burger menu
     const burgerMenu = document.querySelector(".burger");
     burgerMenu.addEventListener('click', () => {
         document.getElementById("nav__container").classList.toggle("nav-show")
     })
-
-
-
-
-
-
-// Footer menu 
+    // Footer menu 
     const footerMenuItems = document.querySelectorAll(".js-footer-menu");
     footerMenuItems.forEach(menuItem => {
         menuItem.addEventListener('click', () => {
             menuItem.nextElementSibling.classList.toggle("hidden")
         });
     });
+    // chargement aléatoir des wallpaper
+    const listWallpaper = [
+        "battle.png",
+        "mario-luigi.jpg",
+        "minecraft.png",
+        "monstre.jpg"
+    ] 
+
+    const imageSrc = listWallpaper[Math.floor(Math.random() * listWallpaper.length)];
+    document.body.style.backgroundImage = `url("assets/img/wallpapers/${imageSrc}")`;
 });
