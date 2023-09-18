7e01229a9080551ee072447a7cb4984da228a136
tbressel <tbressel.dev@gmail.com>
2023-09-19 00:35:48 +0200

    adding a drawer in items information

--- components/collection-view-2/collection-view-2.js
+++ components/collection-view-2/collection-view-2.js
@@ -1,4 +1,17 @@
-   document.getElementById("collection-view-2").addEventListener('click', (event) => {
-      if(event.target.getAttribute("id") !== "arrow-img") return 
-    console.log('ok')
-   });
+document.getElementById("collection-view-2").addEventListener('click', (event) => {
+   if (event.target.getAttribute("id") === "arrow-img") {
+      event.target.classList.toggle("arrow_change");
+      const newTargetElement = event.target.parentElement.parentElement.nextElementSibling;  
+      newTargetElement.classList.toggle('hidden');
+   }
+
+   if (event.target.getAttribute("id") === "edit-notes") {
+   const newTargetElement = event.target.parentElement.parentElement.parentElement.parentElement.nextElementSibling;
+   console.log(newTargetElement)
+      newTargetElement.classList.toggle('hidden');
+   };
+});
+
+
+
+
--- components/collection-view-2/collection-view-2.php
+++ components/collection-view-2/collection-view-2.php
@@ -11,20 +11,62 @@
 
       foreach ($req_results as $result) {
          echo '<div class="collection-item__maincontainer">
-   <div class="select__bar">
-   <input type="checkbox" name="selected" id=""></div>
-   <div class="collection-items__coverimage">
-   <img src="./assets/' . $result[5] . '" alt=""></div>
-   <div class="collection-basicinfos__container">
-   <div class="collection-items__title"><h3>' . $result[1] . ($result[2] ? ' - ' : '') . $result[2] . '</h3></div>
-   <div class="collection-basicinfos__subcontainer">
-   <div class="collection-items__plateform"><p>' . $result[3] . '</p></div>
-   <div class="collection-items__editor"><p>' . $result[7] . '</p></div>
-   <div class="collection-items__year"><p>' . $result[6] . '</p></div>
-   </div>
-   </div><div class="arrow"><img id="arrow-img" src="./assets/svg/chevron-up-solid.svg" alt="button"></div>
-   </div>
-   </div>';
+                  <div class="top-informations">
+                     <div class="select__bar">
+                        <input type="checkbox" name="selected" id="">
+                     </div>
+                     <div class="collection-items__coverimage">
+                        <img src="./assets/' . $result[5] . '" alt="">
+                     </div>
+
+                     <div class="collection-basicinfos__container">
+                           <div class="collection-items__title">
+                              <h3>' . $result[1] . ($result[2] ? ' - ' : '') . $result[2] . '</h3>
+                           </div>
+                        <div class="collection-basicinfos__subcontainer">
+                           <div class="collection-items__plateform">
+                              <p>' . $result[3] . '</p>
+                           </div>
+                           <div class="collection-items__editor">
+                                 <p>' . $result[7] . '</p>
+                           </div>
+                           <div class="collection-items__year">
+                                 <p>' . $result[6] . '</p>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="arrow">
+                        <img id="arrow-img" src="./assets/svg/chevron-up-solid.svg" alt="">
+                     </div>
+                  </div>
+                  <div id="collection-items-bot" class="bot-informations hidden">
+                     <div class="select__bar"></div>
+                        <div class="bot-items__subcontainer">
+                           <div class="bot-items__prices">
+                              <h4>Minimuim<span>€100,50</span></h4>
+                              <h4>Moyen<span>€60,78</span></h4>
+                              <h4>Maximum<span>€345,78</span></h4>
+                           </div>
+
+                           <div class="bot-items__date-state">
+                              <h5>Ajouté <span>il y a 1 ans et 6 mois</span></h5>
+                              <p>Très bon état - presque Mint</p>
+                           </div>
+                           <div>
+                              <h4>Notes <span id="edit-notes"> Editer les notes </span></h4>
+                           </div>
+                        </div>
+                     </div>
+                  <div id="collection-notes" class="note-informations hidden">
+                     <div class="select__bar"></div>
+                        <form class="form-notes" method="get" action="#" name"text-notes">
+                           <label htmlfor="input-notes">
+                              <input id="input-notes" class="input-notes" name="notes" type="text">
+                           </label>
+                        </form>
+                     </div>
+                  </div>   
+               </div>';
       }
       $connexion = null;
       ?>
